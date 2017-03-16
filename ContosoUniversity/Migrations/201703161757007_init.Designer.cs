afb7d41626e18825bab694364e118da42e3f65e4
Phan Dinh Van <Phan Dinh Van>
2017-03-17 01:15:24 +0700

    DinhVan

--- ContosoUniversity/Migrations/201703161757007_init.Designer.cs
+++ ContosoUniversity/Migrations/201703161757007_init.Designer.cs
@@ -7,13 +7,13 @@ namespace ContosoUniversity.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.1.3-40302")]
-    public sealed partial class InitialCreate : IMigrationMetadata
+    public sealed partial class init : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(InitialCreate));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(init));
         
         string IMigrationMetadata.Id
         {
-            get { return "201703160824134_InitialCreate"; }
+            get { return "201703161757007_init"; }
         }
         
         string IMigrationMetadata.Source
